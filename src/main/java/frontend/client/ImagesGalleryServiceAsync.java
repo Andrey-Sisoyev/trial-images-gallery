f721694ccbeba3c69d925a0ba786ca5fa49c9d55
Andrey Sisoyev <sisoyev.andrey@yandex.ru>
2011-06-06 01:31:00 +0300

    Release of v1.3: cleaner code

--- src/main/java/frontend/client/ImagesGalleryServiceAsync.java
+++ src/main/java/frontend/client/ImagesGalleryServiceAsync.java
@@ -3,13 +3,24 @@ package frontend.client;
 import java.util.List;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import utils.ConfiguredConstraintsViolatedException;
+import utils.EntityExistsNotException;
+import utils.InvalidDataException;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import frontend.shared.ImageFEObject;
 
 public interface ImagesGalleryServiceAsync {
-    void findByPK(Integer id, AsyncCallback<ImageFEObject> callback);
-    void findFirst(AsyncCallback<ImageFEObject> callback);
-    void findAllSorted(AsyncCallback<List<ImageFEObject>> callback);
-    void create(ImageFEObject entity, AsyncCallback<Integer> callback);
-    void deleteByPK(Integer pk, AsyncCallback<ImageFEObject> callback);
+    void findByPK(Integer id, AsyncCallback<ImageFEObject> async);
+
+    void findFirst(AsyncCallback<ImageFEObject> async);
+
+    void findAllSorted(AsyncCallback<List<ImageFEObject>> async);
+
+    void create(ImageFEObject entity, AsyncCallback<Integer> async);
+
+    void deleteByPK(Integer pk, AsyncCallback<ImageFEObject> async);
 }
